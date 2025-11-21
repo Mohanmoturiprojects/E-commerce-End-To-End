93a5f6eb0bb9d63850fdaf58986661157de4d133
Mohanmoturiprojects <mohanmoturi43@gmail.com>
2025-11-21 11:15:22 +0530

    Update HomeFurniture.jsx

--- Front-end/LogikalMArt/src/HomeFurniture.jsx
+++ Front-end/LogikalMArt/src/HomeFurniture.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import ProductCard from "./ProductCard";
+import "./Electronics.css";
 
 const HomeFurniture = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8081/products/categoryparent/HomeFurniture")
+      .then(res => setProducts(res.data))
+      .catch(err => console.log(err));
+  }, []);
+
   return (
-    <div>
-     <h1>welcome to Home & Furniture Logical mart</h1> 
+    <div className="electronics-page">
+      <h2 className="title">HomeFurniture</h2>
+
+      <div className="products-grid">
+        {products.map((p) => (
+          <ProductCard key={p.id} product={p} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default HomeFurniture
